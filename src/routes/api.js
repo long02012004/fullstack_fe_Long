40191f158a249f8a4f4285815a0380cd74ec14cf
qLong <nguyenquanglong100285@gmail.com>
2026-05-25 14:06:36 +0700

    Feat: Khởi tạo project hoặc lưu code hiện tại

---
+++ src/routes/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const routerAPI = express.Router();
+
+// const { getUsersAPI, postCreateUserAPI,
+//     putUpdateUserAPI, deleteUserAPI
+
+// } = require('../controllers/apiController')
+
+// routerAPI.get('/users', getUsersAPI);
+// routerAPI.post('/users', postCreateUserAPI);
+// routerAPI.put('/users', putUpdateUserAPI);
+// routerAPI.delete('/users', deleteUserAPI);
+routerAPI.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello from API" });
+});
+
+module.exports = routerAPI; //export default
