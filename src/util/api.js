79f49b6c83a8ddd8e5a074b2028a8d817e8595a6
qLong <nguyenquanglong100285@gmail.com>
2026-05-30 10:37:59 +0700

    đính kèm access_token vào header

--- src/util/api.js
+++ src/util/api.js
@@ -1,17 +1,23 @@
 import axiosInstance from "./axios.customize";
 
+// api đăng ký
 const createUser = (name, email, password) => {
   const URL_API = "/v1/api/register";
-
   const data = { name, email, password };
   return axiosInstance.post(URL_API, data);
 };
 
+// api đăng nhập
 const loginUser = (email, password) => {
   const URL_API = "/v1/api/login";
   const data = { email, password };
   return axiosInstance.post(URL_API, data);
 };
+// api lấy danh sách người dùng\
+const getUsers = () => {
+  const URL_API = "/v1/api/users";
+  return axiosInstance.get(URL_API);
+}
 
-export { createUser, loginUser };
+export { createUser, loginUser, getUsers };
 
--- src/util/axios.customize.js
+++ src/util/axios.customize.js
@@ -2,24 +2,46 @@ import axios from "axios";
 
 //custom axios instance
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,  
-})
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+});
 
 // Request interceptors
-instance.interceptors.request.use((config) =>{
-    return config
-}, (error) =>{
-    return Promise.reject(error)
-})
+// Request interceptors
+instance.interceptors.request.use(
+  (config) => {
+    // 1. Lấy token từ localStorage
+    const token = localStorage.getItem("access_token");
+
+    // 2. Kiểm tra nếu có token thì gán trực tiếp vào config.headers
+    if (token) {
+      // Đảm bảo config.headers không bị undefined (phòng hờ)
+      config.headers = config.headers || {};
+
+      // Gán token theo chuẩn Bearer (thường các backend dùng chuẩn này)
+      config.headers["Authorization"] = `Bearer ${token}`;
+
+      // Hoặc nếu backend của bạn chỉ nhận token thuần (không có chữ Bearer) thì dùng dòng dưới:
+      // config.headers["Authorization"] = token;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 // Response interceptors
-instance.interceptors.response.use((response) =>{
-    if(response && response.data){
-        return response.data
+instance.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
     }
-    return response
-}, (error) =>{
-    return Promise.reject(error)
-})
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
-export default instance
+export default instance;
